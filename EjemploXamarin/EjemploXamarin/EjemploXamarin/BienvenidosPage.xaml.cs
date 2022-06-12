95d7545bc4203f6caa852952e2c8a461e0149a0a
Jesus <jemoar8@gmail.com>
2022-06-12 15:39:47 +0200

    Aplicacion de ejemplo de Xamarin y Xamarin Forms

---
+++ EjemploXamarin/EjemploXamarin/EjemploXamarin/BienvenidosPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EjemploXamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BienvenidosPage : ContentPage
+    {
+        public BienvenidosPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Título", "¡Bienvenido a esta prueba de Xamarin!", "Cancelar");
+        }
+    }
+}
